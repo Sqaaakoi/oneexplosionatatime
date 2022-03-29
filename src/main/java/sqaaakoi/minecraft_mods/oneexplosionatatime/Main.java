9cd04114b303ab9dede2559fd5f1efd05ea9665a
Sqaaakoi <37475903+Sqaaakoi@users.noreply.github.com>
2022-03-29 17:25:57 +1300

    setup dev env

--- src/main/java/sqaaakoi/minecraft_mods/oneexplosionatatime/Main.java
+++ src/main/java/sqaaakoi/minecraft_mods/oneexplosionatatime/Main.java
@@ -1,11 +1,11 @@
-package example.minecraft_mods.template;
+package sqaaakoi.minecraft_mods.oneexplosionatatime;
 
 import net.fabricmc.api.ModInitializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Main implements ModInitializer {
-  public static final String ID = "template";
+  public static final String ID = "oneexplosionatatime";
   public static final Logger LOG = LogManager.getLogger(ID);
 
   @Override
